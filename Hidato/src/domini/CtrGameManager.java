9cddfce3b6b39992f9ee5fc7eac91e3d834df46f
Pau <Pau@Pau-PC>
2015-11-13 19:24:29 +0100

    classes de la partida comentades be

--- Hidato/src/domini/CtrGameManager.java
+++ Hidato/src/domini/CtrGameManager.java
@@ -7,22 +7,26 @@ package domini;
  */
 
 /**
- *
+ * Controlador de totes les partides. Quan la vista vol crear/recuperar una partida,
+ * aquest controlador li proporciona el controlador de partida corresponent. Hi ha
+ * un d'aquests controladors per cada usuari loguejat -> si es fa logout i login
+ * amb un altre usuari, s'ha de crear un altre objecte CtrGameManager
  * @author Pau Surrell
  */
 
-
-/**
- * Un d'aquests controladors per cada usuari loguejat -> si es fa logout i login
- * amb un altre usuari, s'ha de crear un altre objecte CtrGameManager
- */
 public class CtrGameManager {
+    /**
+     * Declara els parametres i controladors de la classe
+     */
     private final HidatoSet hidatoSet;
-    private User loggedUser;
+    private final User loggedUser;
     private final CtrDBGame ctrDBGame;
     private final Solver solver;
-    private CtrHidato ctrHidato;
+    private final CtrHidato ctrHidato;
     
+    /**
+     * Creadora del controlador
+     */
     public CtrGameManager(HidatoSet hidatoSet, User loggedUser, CtrDBGame ctrDBGame, Solver solver, CtrHidato ctrHidato){
         this.hidatoSet = hidatoSet;
         this.loggedUser = loggedUser;
@@ -31,19 +35,35 @@ public class CtrGameManager {
         this.ctrHidato = ctrHidato;
     }
     
-    public CtrCurrentGame createGame(String name, String hidatoName, String help, String difficulty){
+    /**
+     * Crea una partida
+     * @param name Nom de la partida
+     * @param hidatoName Nom del tauler sobre el qual es crea la partida
+     * @param help Nivell d'ajuda
+     * @return el controlador de la partida creada
+     */
+    
+    public CtrCurrentGame createGame(String name, String hidatoName, Help help){
         Hidato hidato = hidatoSet.getHidatoByName(hidatoName);
-        Game game = new Game(name, hidato, loggedUser, help, difficulty);
+        Game game = new Game(name, hidato, loggedUser, help, hidato.getDifficult());
         CtrCurrentGame ctrCurrentGame = new CtrCurrentGame(game, ctrDBGame, solver, ctrHidato); //qui passa el ctr hidato?
         return ctrCurrentGame;
     }
-  
+    /**
+     * Recupera una partida que s'havia guardat amb nom 'name'
+     * @return el controlador de la partida recuperada
+     */
     public CtrCurrentGame restoreGame(String name){
         Game game = ctrDBGame.getGame(name, loggedUser);
         ctrDBGame.deleteGame(name, loggedUser);
         CtrCurrentGame ctrCurrentGame = new CtrCurrentGame(game, ctrDBGame, solver, ctrHidato);
         return ctrCurrentGame;
     }
+    
+    /**
+     * Elimina la partida del repositori amb nom 'name' i usuari 'loggedUser'
+     * @return 0
+     */
     
     public int deleteGame(String name){
         ctrDBGame.deleteGame(name, loggedUser);
