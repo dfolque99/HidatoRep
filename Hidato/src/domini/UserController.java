17ed461fedb41c33fa988868ff3ef5cde37e9191
Guillem <Guillem@10.182.91.226>
2015-11-13 19:35:59 +0100

    Modif UserController

--- Hidato/src/domini/UserController.java
+++ Hidato/src/domini/UserController.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 
-import UserDBController;
-
 /**
  *
  * @author Guillem
@@ -29,33 +27,43 @@ public class UserController {
         return 0;
     }
 
-    //Hay que cambiar la especificacion de createUser
+    
     public int createUser(String username, String password) {
+        UserDBController userDBController = new UserDBController();
         User newUser = new User(username,password);
-        int error = UserDBController.createUser(newUser);
+        int error = userDBController.createUser(newUser);
         return error;
     }
     
     public int deleteUser(String password) {
+        UserDBController userDBController = new UserDBController();
         if (!password.equals(loggedUser.getPassword())) return -1;
-        if (UserDBController.deleteUser(loggedUser.getName()) != 0) return -2;
+        if (userDBController.deleteUser(loggedUser.getName()) != 0) return -2;
         loggedUser = null;
         return 0;
+    }
+    
+    public int updateUser() {
+        UserDBController userDBController = new UserDBController();
+        int error = userDBController.modifyUser(loggedUser);
+        return error;
     }
 
     public User getLoggedUser() {
         return loggedUser;
     }    
     public User getUser(String username) {
-        return UserDBController.getUser(username);
+        UserDBController userDBController = new UserDBController();
+        return userDBController.getUser(username);
     }
     
     public int modifyName(String password, String newName) {
+        UserDBController userDBController = new UserDBController();
         if (!password.equals(loggedUser.getPassword())) return -1;
-        if (UserDBController.getUser(newName) != null) return -2;
+        if (userDBController.getUser(newName) != null) return -2;
         String oldName = new String(loggedUser.getName());
         loggedUser.setName(newName);
-        int error = UserDBController.modifyUser(loggedUser,loggedUser.getName());
+        int error = userDBController.modifyUser(loggedUser,loggedUser.getName());
         if (error != 0) {
             loggedUser.setName(oldName);
             return -3;
@@ -65,9 +73,10 @@ public class UserController {
     
     
     public int modifyPassword(String oldPassword, String newPassword) {
+        UserDBController userDBController = new UserDBController();
         if (!oldPassword.equals(loggedUser.getPassword())) return -1;
         loggedUser.setPassword(newPassword);
-        int error = UserDBController.modifyUser(loggedUser,loggedUser.getName());
+        int error = userDBController.modifyUser(loggedUser,loggedUser.getName());
         if (error != 0) {
             loggedUser.setPassword(oldPassword);
             return -2;
