1d1a636d1ebcfb3638c106f07692fa6bdad75a65
Félix Axel Gimeno Gil <keur@users.noreply.github.com>
2015-11-14 17:45:30 +0100

    Update CtrStatsUser.java

--- Hidato/src/domini/CtrStatsUser.java
+++ Hidato/src/domini/CtrStatsUser.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domini;
-
+import java.time.Duration;
 /**
  *
  * @author felix.axel.gimeno
  */
 public class CtrStatsUser {
+    private final HidatoUser user;
     
+    private CtrStatsUser(){throw new UnsupportedOperationException();}
+    public CtrStatsUser(HidatoUser A){this.user = A;}
+    
+    public int getSolvedGames(){ return user.getSolvedGames(); }
+    public int getStartedGames(){ return user.getStartedGames(); }
+    public Duration getTotalTimePlayed(){ return user.getTotalTimePlayed(); }
 }
