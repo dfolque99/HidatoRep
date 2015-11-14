d06fe50ba775b96978bbbb8ad3d4c44f5984403a
Guillem <Guillem@GuillemPC>
2015-11-14 02:09:41 +0100

    CtrRanking added

---
+++ Hidato/src/domini/CtrRanking.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domini;
+
+/**
+ *
+ * @author Guillem
+ */
+public class CtrRanking {
+    
+    private Ranking easyRanking;
+    private Ranking mediumRanking;
+    private Ranking hardRanking;
+    
+    void addEntryToRanking(int score, String username, Difficulty difficulty) {
+        RankingEntry newEntry = new RankingEntry(,username,difficulty);
+        
+    }
+    
+}
