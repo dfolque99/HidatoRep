6e7d940c54a1b36c5d5050e126c6bb21c514a5a1
Pau <Pau@192.168.1.142>
2015-11-14 11:31:01 +0100

    acabat comunicacio amb ranking

--- Hidato/src/domini/CtrCurrentGame.java
+++ Hidato/src/domini/CtrCurrentGame.java
@@ -20,21 +20,24 @@ public class CtrCurrentGame {
      * Declaracio dels parametres i controladors amb els que necessita comunicar-se
      */
     private final Game game;
-    private CtrDBGame ctrDBGame;
-    private Solver solver;
-    private CtrHidato ctrHidato;
+    private final CtrDBGame ctrDBGame;
+    private final Solver solver;
+    private final CtrHidato ctrHidato;
+    private final CtrRanking ctrRanking;
     private long time0;
+    
     
     /**
      * Creadora
      * Crea un controlador a partir d'un game, un CtrDBGame (que permet la comunicacio
      * amb la capa de persistencia) i un solver
      */    
-    public CtrCurrentGame(Game game, CtrDBGame ctrDBGame, Solver solver){
+    public CtrCurrentGame(Game game, CtrDBGame ctrDBGame, Solver solver, CtrRanking ctrRanking){
         this.game = game;
         this.ctrDBGame = ctrDBGame;
         this.ctrHidato = new CtrHidato(game.getHidato());
         this.solver = solver;
+        this.ctrRanking = ctrRanking;
     }
 
     /**
@@ -79,9 +82,7 @@ public class CtrCurrentGame {
             if (ctrHidato.AreCellsContiguous(value, value+1) && ctrHidato.AreCellsContiguous(value, value-1)){
                 return 0;
             }else return -2; // -2 = HI HA DOS NOMBRES CONSECUTIUS SEPARATS
-            
-        }
-        return 0;
+        }else return 0;
     }
     
     /**
@@ -121,14 +122,17 @@ public class CtrCurrentGame {
     public int finishGame(){
         long time1 = System.currentTimeMillis();
         game.incrementDuration(time1 - time0);
-        User user_aux = game.getUser();
-        user_aux.IncrementSolvedHidatos();
-        user_aux.IncrementTimePlayed(game.getDuration());
-        int score = calculateScore();
-        user_aux.IncrementTotalScore(score);
-        RankingEntry entry = new RankingEntry(game.getDate(), user_aux.getUsername(), score);
         
-        //FALTA RANKING
+        User user = game.getUser();
+        user.IncrementSolvedHidatos();
+        user.IncrementTimePlayed(game.getDuration());
+        String username = user.getUsername();
+        
+        int score = calculateScore();
+        user.IncrementTotalScore(score);
+        
+        RankingEntry entry = new RankingEntry(game.getDate(), username, score);
+        ctrRanking.addEntryToRanking(score, username, game.getDifficulty());
         return 0;
     }
     
@@ -137,7 +141,11 @@ public class CtrCurrentGame {
      * @return la puntuacio
      */
     public int calculateScore(){
-        return 9001;
+        int checkPenalty = 60000;
+        int changePenalty = 1000;
+        long totalDuration = game.getDuration().toMillis() + checkPenalty*game.getChecksMade() + changePenalty * game.getChangesMade();
+        long score = (long) (1e10 / totalDuration);
+        return (int) score;
     }
     
     /**
