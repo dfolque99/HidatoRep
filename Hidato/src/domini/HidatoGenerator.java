8ea6fcc63d01f9b1276b5efbdf534f1e640a1dc7
David <David@Lenovo>
2015-11-14 14:53:00 +0100

    David

--- Hidato/src/domini/HidatoGenerator.java
+++ Hidato/src/domini/HidatoGenerator.java
@@ -5,16 +5,22 @@
  */
 package domini;
 
+
 import java.util.LinkedList;
 
 /**
  *
  * @author David
+ * 
+ * pendent:
+ *      treure R[i][j]
+ *      que el particionat no redueixi la meitat, sino nomes fins que no ho estigui
  */
 public class HidatoGenerator {
     
     private Hidato h;
     private int n, m;
+    private int iter, val_ref;
     private int R[][]; // s'ha d'eliminar
     private int D[][];
     private int totalCaselles;
@@ -26,6 +32,7 @@ public class HidatoGenerator {
         R = new int[sizeX][sizeY]; // canviar
         comptaCaselles();
     }
+    
     public HidatoGenerator(Hidato h) {
         this.h = new Hidato(h);
         n = h.getSizeX();
@@ -45,8 +52,15 @@ public class HidatoGenerator {
         retorna null si no hi ha casella inicial o si es impossible de generar
     */
     public Hidato generateHidato(String difficulty) {
-        if (noCasellaInicial()) return null;
         if (completarCami() == false) return null;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                System.out.printf("%d ", R[i][j]);
+                if (R[i][j] < 10) System.out.printf(" ");
+                if (R[i][j] < 100) System.out.printf(" ");
+            }
+            System.out.printf("\n");
+        }
         posarPistes(difficulty);
         return h;
     }
@@ -56,25 +70,105 @@ public class HidatoGenerator {
     
     /*============================== PRIVADES ================================*/
     
+    private boolean completarCami() {
+        Position casellaInicial = buscaCasellaInicial();
+        if (casellaInicial == null) return false;
+        omplirD();
+        
+        iter = 0;
+        val_ref = 0;
+        backtracking(1, casellaInicial);
+        
+        return true;
+    }
+    
+    private boolean backtracking (int val, Position p) {
+        if (val == totalCaselles) return true;
+        if (!controlParticionament(val)) return false;
+        Position veiObligat = buscaVeiObligat(val, p);
+        if (veiObligat != null) return backtracking(val+1, veiObligat);
+        LinkedList<Position> veins = buscaVeins(val, p);
+        if (veins.size() == 0) return false;
+        int random;
+        if (probabilitatCamiSegur()) {
+            veins.sort((p1, p2) -> D[p1.x][p1.y] - D[p2.x][p2.y]);
+            random = 0;
+        }
+        else random = randomNum(0,veins.size()-1);
+        for (int i = 0; i < veins.size(); ++i) {
+            Position next = veins.get((i+random)%veins.size());
+            int antVal = R[next.x][next.y];
+            R[next.x][next.y] = val+1;
+            sumaVoltantD(next.x,next.y,-1);
+            boolean result = backtracking(val+1,next);
+            if (result) return true;
+            sumaVoltantD(next.x,next.y,1);
+            R[next.x][next.y] = antVal;
+            if (val_ref != 0) {
+                if (val > val_ref/2) return false;
+                if (particionat(val)) {
+                    val_ref = val;
+                    return false;
+                }
+                val_ref = 0;
+            }
+        }
+        return false;
+    }
+    
+    private boolean probabilitatCamiSegur() {
+        return randomNum(1,100) <= 40; // probabilitat del 40%
+    }
+    
+    private LinkedList<Position> buscaVeins(int val, Position p) {
+        LinkedList<Position> ret = new LinkedList<>();
+        for (int i = Math.max(p.x-1, 0); i <= Math.min(p.x+1, n-1); ++i) {
+            for (int j = Math.max(p.y-1, 0); j <= Math.min(p.y+1, m-1); ++j) {
+                if (i == p.x && j == p.y) continue;
+                if (R[i][j] == 0 && !h.getCell(i,j).getType().equals("No Valida")) {
+                    ret.addLast(new Position(i,j));
+                }
+            }
+        }
+        return ret;
+    }
+    
+    private Position buscaVeiObligat(int val, Position p) {
+        for (int i = Math.max(p.x-1, 0); i <= Math.min(p.x+1, n-1); ++i) {
+            for (int j = Math.max(p.y-1, 0); j <= Math.min(p.y+1, m-1); ++j) {
+                if (i == p.x && j == p.y) continue;
+                if (R[i][j] == val+1 && !h.getCell(i,j).getType().equals("No Valida")) {
+                    return new Position(i,j);
+                }
+            }
+        }
+        return null;
+    }
+    
+    private boolean controlParticionament(int val) {
+        if (++iter == 1000) {
+            iter = 0;
+            if (particionat(val-1)) {
+                val_ref = val;
+                return false;
+            }
+        }
+        return true;
+    }
+    
     private class Position {
         int x;
         int y;
         public Position(int x, int y) {this.x = x; this.y = y;}
     }
     
-    private boolean noCasellaInicial() {
-        boolean ret = true;
+    private Position buscaCasellaInicial() {
         for (int i = 0; i < n; ++i){
             for (int j = 0; j < m; ++j) {
-                if (h.getCell(i,j).getVal() == 1) ret = false;
+                if (h.getCell(i,j).getVal() == 1) return new Position(i,j);
             }
         }
-        return ret;
-    }
-    
-    private boolean completarCami() {
-        omplirD();
-        return true;
+        return null;
     }
     
     public boolean particionat(int val) {
@@ -158,6 +252,10 @@ public class HidatoGenerator {
                 if(R[i][j] == 0) sumaVoltantD(i,j,1);
             }
         }
+    }
+    
+    private int randomNum (int min, int max) {
+        return (int)(Math.random() * (max - min + 1)) + min;
     }
     
     
