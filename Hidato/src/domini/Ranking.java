63413517b69255072fc5d46007febd057e1e29c5
Guillem <Guillem@GuillemPC>
2015-11-14 01:34:14 +0100

    Ranking + RankingEntry

--- Hidato/src/domini/Ranking.java
+++ Hidato/src/domini/Ranking.java
@@ -12,28 +12,53 @@ import java.util.ArrayList;
  * @author Guillem
  */
 public class Ranking {
+
     private static final int N = 20;
     private final Difficulty difficulty;
-    private ArrayList <RankingEntry> ranking;
-    
+    private ArrayList<RankingEntry> ranking;
+
     public Ranking(Difficulty difficulty) {
         this.difficulty = difficulty;
-        ranking = new ArrayList<>();
+        ranking = new ArrayList<>(N+1);
+        ranking.ensureCapacity(N+1);
     }
-    
-    public void insertEntry(RankingEntry newEntry);
-    
-    public int getMaxSize() {
-        return N;
-    }
-    
+
     public int getSize() {
         return ranking.size();
     }
-    
-    public getDifficulty() {
+
+    public int getMaxSize() {
+        return N;
+    }
+
+    public Difficulty getDifficulty() {
         return difficulty;
     }
-    
-    
+
+    public RankingEntry get(int i) {
+        if (0 <= i && i < ranking.size()) {
+            return ranking.get(i);
+        }
+        return null;
+    }
+
+    public void addRankingEntry(RankingEntry newEntry) {
+        int newScore = newEntry.getScore();
+        int l = 0;
+        int r = ranking.size() - 1;
+        int pos = r + 1;
+        while (l <= r) {
+            int m = (l+r)/2;
+            int score = ranking.get(m).getScore();
+            if (newScore < score) l = m + 1;
+            else if (newScore > score) {
+                pos = m;
+                r = m - 1;
+            }
+            else l = m + 1;
+        }
+        ranking.add(pos,newEntry);
+        if (ranking.size() > N) ranking.remove(N);
+    }
+
 }
