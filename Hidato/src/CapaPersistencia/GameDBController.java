587c23c4e34facb7cd20f2cf05b5f0c21231891e
Guillem <Guillem@GuillemPC>
2015-12-10 23:48:49 +0100

    GameDBController implementado (falta testear)

--- Hidato/src/CapaPersistencia/GameDBController.java
+++ Hidato/src/CapaPersistencia/GameDBController.java
@@ -3,26 +3,94 @@ package CapaPersistencia;
 import CapaDomini.Partida.Game;
 import CapaDomini.Partida.GameSet;
 import CapaDomini.Usuari.HidatoUser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
- * Stub del controlador DB de la partida: hauria de proporcionar la informacio de les partides
- * a la capa de domini. Actualment conte unicament les capcaleres per evitar
- * que les classes de la capa de domini tinguin errors de compilacio
- * @author Pau
+ * @author Guillem
  */
 public class GameDBController {
+    
+    private final static String directory = "Games/";
+    private final static String extension = ".obj";
 
-    public void saveGame(Game game) {}
-
-    public Game getGame(String name, String username) {
-        return null;
+    public void saveGame(Game game) {
+        //Crea la carpeta raiz "Games/" (si no estaba ya hecho)
+        File dir = new File(directory);
+        dir.mkdir();
+        //Crea la subcarpeta "Games/<nombreusuario>/" (si no estaba ya hecho)
+        dir = new File(directory + getFolder(game));
+        dir.mkdir();
+        try {
+            FileOutputStream fos = new FileOutputStream(getDirectory(game));
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(game);
+            fos.close();
+        } catch (Exception e) {
+            
+        }
     }
 
-    public void deleteGame(String name, String username) {}
+    public Game getGame(String gameName, String username) {
+        Game ret = null;
+        try {
+            FileInputStream fis = new FileInputStream(getDirectory(gameName,username));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ret = (Game)ois.readObject();
+            fis.close();
+        }
+        catch (Exception  e) {
+            
+        }
+        finally {
+            return ret;
+        }
+    }
+
+    public void deleteGame(String gameName, String username) {
+        File f = new File(getDirectory(gameName,username));
+        f.delete();
+    }
     
     public ArrayList<Game> getAllGames(String username){
-        return null;
+        ArrayList<Game> ret = new ArrayList();
+        File f = new File(directory + username);
+        ArrayList<String> games = new ArrayList(Arrays.asList(f.list()));
+        for (int i = 0; i < games.size(); ++i) {
+            try {
+                FileInputStream fis = new FileInputStream(directory + username + "/" + games.get(i));
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                ret.add((Game)ois.readObject());
+                fis.close();
+            }
+            catch (Exception e) {
+                
+            }
+            
+        }
+        return ret;
+    }
+    
+    
+    private static String getDirectory(Game game) {
+        return getDirectory(getFolder(game),getName(game));
+    }
+    
+    private static String getDirectory(String gameName, String username) {
+        return directory + username + "/" + gameName + extension;
+    }
+    
+    private static String getFolder(Game game) {
+        return game.getUser().getUsername();
+    }
+    
+    private static String getName(Game game) {
+        return game.getName();
     }
     
 }
