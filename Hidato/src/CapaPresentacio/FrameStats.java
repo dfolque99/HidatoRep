7b642a67f5bed148e3cc8176ce50d2d5f637112a
keur <keur@10.182.72.89>
2015-12-11 16:26:23 +0100

    FrameStats a catala

--- Hidato/src/CapaPresentacio/FrameStats.java
+++ Hidato/src/CapaPresentacio/FrameStats.java
@@ -1,9 +1,6 @@
 package CapaPresentacio;
 import CapaDomini.Misc.Fonts;
 import CapaDomini.Tauler.HidatoManagerController;
-import CapaDomini.Tauler.HidatoSet;
-import CapaDomini.Usuari.HidatoUser;
-import CapaDomini.Usuari.HidatoUserController;
 import CapaDomini.Usuari.UserController;
 import CapaDomini.Usuari.UserStatsController;
 import java.awt.Font;
@@ -17,17 +14,17 @@ import javax.swing.JOptionPane;
  * To use do: new FrameStats(uc,hmc).setVisible(true); 
  * where uc is a valid usercontroller
  * 
- * TO DO: getStats beautify, 
- * 
  * @author felix.axel.gimeno
- * @version 0.28
+ * @version 0.30
  * @since 05-12-2015
  */
 @SuppressWarnings("serial")
-public final class FrameStats extends javax.swing.JFrame {
+public class FrameStats extends javax.swing.JFrame {
     private final static Integer sizeX = 800; 
-    private final static Integer sizeY = 600; 
-    private static String convertToMultiline(final String orig) {return "<html>" + orig.replaceAll("\n", "<br>");}
+    private final static Integer sizeY = 600;
+    private final static Font myFont = Fonts.getFont("OpenSans-Light", Font.PLAIN, 18);
+    private static final Logger LOG = Logger.getLogger(FrameStats.class.getName());
+    private static String convertToMultiline(final String orig) {return "<html><font face=\"OpenSans-Light\">"+orig.replaceAll("\n", "<br>")+"</font></html>";}
     private static String convertToString(String[] myArray){
         String ret = ""; 
         for (String s : myArray) {ret = ret.concat(s).concat("\n\n");}
@@ -38,25 +35,51 @@ public final class FrameStats extends javax.swing.JFrame {
         if (myUser instanceof CapaDomini.Usuari.HidatoUser){
             CapaDomini.Usuari.HidatoUser myHidatoUser = (CapaDomini.Usuari.HidatoUser) myUser;    
             UserStatsController usc = new UserStatsController(myHidatoUser);
-            return new String[]{"Statistics of user: "+uc.getLoggedUser().getUsername(),
-                "getAverageScore: "+String.valueOf(usc.getAverageScore()),
-                "getAverageTimePerSolve: "+String.valueOf(usc.getAverageTimePerSolve().getSeconds()),
-                "getSolvedGames: "+String.valueOf(usc.getSolvedGames()),
-                "getSolvedPercentage: "+String.valueOf(usc.getSolvedPercentage()),
-                "getStartedGames: "+String.valueOf(usc.getStartedGames()),
-                "getTotalCreatedBoards: "+String.valueOf(usc.getTotalCreatedBoards()),
-                "getTotalScore: "+String.valueOf(usc.getTotalScore()),
-                "getTotalTimePlayed: "+String.valueOf(usc.getTotalTimePlayed().getSeconds())
+            return new String[]{"Estadistiques de l'usuari: "+uc.getLoggedUser().getUsername(),
+                "Puntuació mitjana: "+String.valueOf(usc.getAverageScore()),
+                "Temps mitja per resoldre (en segons): "+String.valueOf(usc.getAverageTimePerSolve().getSeconds()),
+                "Quantitat de hidatos resolts: "+String.valueOf(usc.getSolvedGames()),
+                "Percentatge de hidatos resolts: "+String.valueOf(usc.getSolvedPercentage()),
+                "Quantitat de hidatos començats: "+String.valueOf(usc.getStartedGames()),
+                "Quantitat de hidatos creats: "+String.valueOf(usc.getTotalCreatedBoards()),
+                "Puntuació total: "+String.valueOf(usc.getTotalScore()),
+                "Temps total jugant: "+String.valueOf(usc.getTotalTimePlayed().getSeconds())
             };
         } else {
-            return new String[]{"El cast a HidatoUSer ha fallado"};
+            return new String[]{"El cast a HidatoUSer ha fallat"};
         }
     }
     private static javax.swing.JButton createButton(final String text, final java.awt.event.ActionListener l) {
         javax.swing.JButton myButton = new javax.swing.JButton(text);
-        myButton.setFont(Fonts.getFont("OpenSans-Light", Font.PLAIN, 18));
+        myButton.setFont(myFont);
         myButton.addActionListener(l);
         return myButton;
+    }
+
+    private static String askPassword(javax.swing.JFrame myFrame, String extra) {    
+        javax.swing.JPanel panel = new javax.swing.JPanel();
+        javax.swing.JLabel label = new javax.swing.JLabel("Introdueix contrasenya "+extra+" :");
+        javax.swing.JPasswordField pass = new javax.swing.JPasswordField(20);
+        panel.add(label);
+        panel.add(pass);
+        String[] options = new String[]{"OK", "Cancel"};
+        int option = JOptionPane.showOptionDialog(null, panel, null,
+                JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[1]);
+        if (0 == option) {return new String(pass.getPassword());}
+        return null;
+    }
+
+    private static void buttonDeleteActionPerformed(javax.swing.JFrame myJFrame, final UserController uc) {
+        String Password = FrameStats.askPassword(myJFrame, "actual \ni pressiona ok per borrar l'usuari, \npressiona cancel per cancelar");
+        Boolean truePass = uc.getLoggedUser().getPassword().equals(Password);
+        if (truePass) {
+            uc.deleteUser(Password);
+            JOptionPane.showMessageDialog(myJFrame,"Usuari borrat","Usuari borrat",JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else if (null != Password) {
+            JOptionPane.showMessageDialog(myJFrame,"Contraseñya incorrecta","Contraseñya incorrecta",JOptionPane.ERROR_MESSAGE);
+        }
     }
     private HidatoManagerController hmc;
     private CapaDomini.Domini parent;
@@ -64,88 +87,57 @@ public final class FrameStats extends javax.swing.JFrame {
     private FrameStats(){throw new UnsupportedOperationException();}
     
     public FrameStats(CapaDomini.Domini parent, final UserController uc, HidatoManagerController hmc){
-        super("Profile management use case");
+        super("Gestió del perfil de l'usuarie");
         this.parent = parent;
         this.hmc = hmc;
         this.parent = parent;
         this.initComponents(uc);
     }
-    
-    private static void buttonDeleteActionPerformed(javax.swing.JFrame myJFrame,final UserController uc){
-        String Password = JOptionPane.showInputDialog(myJFrame,"Introduce current password \nand press okay to delete current logged user, \npress cancel to cancel",null);
-        Boolean truePass = uc.getLoggedUser().getPassword().equals(Password);
-        if (truePass) {
-            uc.deleteUser(Password);
-            JOptionPane.showMessageDialog(myJFrame,"User deleted","User deleted",JOptionPane.PLAIN_MESSAGE);
-            System.exit(0);
-        } else if (null != Password) {
-            JOptionPane.showMessageDialog(myJFrame,"Incorrect password","Incorrect password",JOptionPane.ERROR_MESSAGE);
-        }
-    }
-    
     private void buttonModifyPasswordActionPerformed(final UserController uc){
-        String OldPassword = JOptionPane.showInputDialog(this,"Introduce current password",null); 
+        String OldPassword = FrameStats.askPassword(this,"actual");
         Boolean truePass = uc.getLoggedUser().getPassword().equals(OldPassword);
         if (truePass) {
-            String NewPassword = JOptionPane.showInputDialog(this,"Introduce new password",null); 
-            uc.modifyPassword(OldPassword, NewPassword);
-            JOptionPane.showMessageDialog(this,"Change done correctly","Change done correctly",JOptionPane.PLAIN_MESSAGE);
+            String NewPassword = FrameStats.askPassword(this,"nova");
+            if (null != NewPassword){
+                uc.modifyPassword(OldPassword, NewPassword);
+                JOptionPane.showMessageDialog(this,"Contraseña canviada","Contraseña canviada",JOptionPane.PLAIN_MESSAGE);
+            }
             
         } else if (null != OldPassword) {
-            JOptionPane.showMessageDialog(this,"Incorrect password","Incorrect password",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this,"Contraseñya incorrecta","Contraseñya incorrecta",JOptionPane.ERROR_MESSAGE);
         }
         
     }
-    private Object[] getHidatos(final UserController uc) {
+    private Object[] getHidatos(final UserController uc){
         String[] ret = hmc.getHidatoList().toArray(new String[]{});
         return ret;
     }
     
-    private void buttonSelectHidatoToEditActionPerformed(final UserController uc) {
+    private void buttonSelectHidatoToEditActionPerformed(final UserController uc){
         Object[] myList = getHidatos(uc);
         
         if (myList.length > 0) {
             //String hidatoName = (String)JOptionPane.showInputDialog(this,"Select Hidato To Edit","Select Hidato To Edit",JOptionPane.QUESTION_MESSAGE, null,myList,myList[0]);
             hidatoName = null;
-            FrameLlista fll = new FrameLlista( new RetornadorString() {
-                @Override
-                public void retorna(String s) {
-                    obreHidato(s);
-                }
-            }, hmc);
+            FrameLlista fll = new FrameLlista( (String s) -> {obreHidato(s);}, hmc);
             fll.setLocation(this.getLocation());
             fll.loadHidatosUsuari();
             fll.setVisible(true);
             this.setVisible(false);
         } else {
-            JOptionPane.showMessageDialog(this,"No hidatos found","The logged user has no hidatos",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(this,"No hidatos trobats","L'usuari no te hidatos guardats",JOptionPane.ERROR_MESSAGE);
         }
     }
-
     private void obreHidato(String s) {
         hidatoName = s;
         if (null != hidatoName) {
-            boolean b = false;
-            try {
-                b = hmc.loadHidato(hidatoName);
-                if (b) {
-                    try {hmc.getTempSizeX();} 
-                    catch (java.lang.NullPointerException e) {LOG.log(Level.SEVERE,"buttonSelectHidatoToEditActionPerformed  hmc.getTempSizeX"); return;}
-                    try {parent.obrirEditor(this);} 
-                    catch (Exception e){
-                        LOG.log(Level.SEVERE,"buttonSelectHidatoToEditActionPerformed ",e); 
-                        JOptionPane.showMessageDialog(this,"There was an error: See log","There was an error: See log",JOptionPane.PLAIN_MESSAGE);
-                        return;
-                    }
-
-                } else {
-                    JOptionPane.showMessageDialog(this,"The selecte Hidato couldn't be loaded correctly","The selecte Hidato couldn't be loaded correctly",JOptionPane.PLAIN_MESSAGE);    
-                }
-            } catch (Exception e) {
-                LOG.log(Level.SEVERE,"Edición de Hidato falló: b "+b,e); return;
+            boolean b = hmc.loadHidato(hidatoName);
+            if (b) {
+                //hmc.getTempSizeX(); 
+                parent.obrirEditor(this); 
+            } else {
+                JOptionPane.showMessageDialog(this,"No se ha pogut carregar","No se ha pogut carregar",JOptionPane.PLAIN_MESSAGE);    
             }
-        } else {
-            //JOptionPane.showMessageDialog(this,"You didn't select anything","You didn't select anything",JOptionPane.PLAIN_MESSAGE);
         }
     }
     
@@ -159,10 +151,10 @@ public final class FrameStats extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         this.getContentPane().setLayout(layout);
         
-        final JButton buttonReturnToMenu = createButton("Return To Menu",(java.awt.event.ActionEvent evt) -> {parent.obrirMenu(this);});   
-        final JButton buttonDelete = createButton("Delete User",(java.awt.event.ActionEvent evt) -> {buttonDeleteActionPerformed(this,uc);});   
-        final JButton buttonModifyPassword = createButton("Modify Password User",(java.awt.event.ActionEvent evt) -> {buttonModifyPasswordActionPerformed(uc);});
-        final JButton buttonSelectHidatoToEdit = createButton("Edit Already Created Hidato", (java.awt.event.ActionEvent evt) -> {buttonSelectHidatoToEditActionPerformed(uc);});
+        final JButton buttonReturnToMenu = createButton("Volver al Menu",(java.awt.event.ActionEvent evt) -> {parent.obrirMenu(this);});
+        final JButton buttonDelete = createButton("Borrar usuari",(java.awt.event.ActionEvent evt) -> {buttonDeleteActionPerformed(this,uc);});   
+        final JButton buttonModifyPassword = createButton("Canviar contrasenya",(java.awt.event.ActionEvent evt) -> {buttonModifyPasswordActionPerformed(uc);});
+        final JButton buttonSelectHidatoToEdit = createButton("Editar hidato guardat", (java.awt.event.ActionEvent evt) -> {buttonSelectHidatoToEditActionPerformed(uc);});
         
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
@@ -177,6 +169,7 @@ public final class FrameStats extends javax.swing.JFrame {
                                 .addComponent(buttonModifyPassword,     javax.swing.GroupLayout.Alignment.CENTER, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(buttonSelectHidatoToEdit, javax.swing.GroupLayout.Alignment.CENTER, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         )
+                        .addGap(30)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
@@ -191,5 +184,5 @@ public final class FrameStats extends javax.swing.JFrame {
     
         this.pack();
     }
-    private static final Logger LOG = Logger.getLogger(FrameStats.class.getName());
+
 }
