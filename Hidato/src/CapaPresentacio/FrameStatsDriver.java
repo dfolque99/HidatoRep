2b9e1568e19b02b47dc5ba18142ece9b456293a1
keur <keur@10.182.72.89>
2015-12-11 16:42:08 +0100

    Style

--- Hidato/src/CapaPresentacio/FrameStatsDriver.java
+++ Hidato/src/CapaPresentacio/FrameStatsDriver.java
@@ -10,49 +10,48 @@ import CapaDomini.Usuari.HidatoUserController;
  * @author felix.axel.gimeno
  */
 public class FrameStatsDriver {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         final String foo = "ghyy";
-        final String name =  "Hidato"+foo+"9";
-        
+        final String name = "Hidato" + foo + "9";
+
         java.awt.EventQueue.invokeLater(() -> {
             HidatoUserController huc;
             HidatoManagerController hmc;
 
             try {
                 huc = new HidatoUserController();
-                
-                huc.createUser(foo,foo);
-                huc.login(foo,foo);
+
+                huc.createUser(foo, foo);
+                huc.login(foo, foo);
                 huc.updateUser();
-                
-                ((HidatoUser)huc.getLoggedUser()).addHidato(name);   
-                
-                hmc = new HidatoManagerController(new HidatoSet(), null, huc); 
-                
+
+                ((HidatoUser) huc.getLoggedUser()).addHidato(name);
+
+                hmc = new HidatoManagerController(new HidatoSet(), null, huc);
+
                 hmc.loadAll();
-                hmc.createRandom(3,3);
+                hmc.createRandom(3, 3);
                 hmc.saveTempHidato(name);
                 hmc.saveAll();
                 hmc.loadAll();
-                
 
                 //((HidatoUser)huc.getLoggedUser()).addHidato("felix");
                 /*CapaDomini.Tauler.Hidato h = new CapaDomini.Tauler.Hidato(5,5);
-                h.setBoardName(name);
-                hs.addHidato(h);
-                */
+                 h.setBoardName(name);
+                 hs.addHidato(h);
+                 */
                 System.out.println(hmc.loadHidato(name));
                 //CapaDomini.Domini d = ;
                 //d.hmc = hmc;
-                new FrameStats(new CapaDomini.Domini(huc,null,null,hmc), huc, hmc).setVisible(true);
-		//hacer new CapaDomini.Domini() hace que FrameEditor tenga hmc distinto del de esta funcion  
-            } catch (Exception e){
-                System.out.println("Couldn't do login");               
+                new FrameStats(new CapaDomini.Domini(huc, null, null, hmc), huc, hmc).setVisible(true);
+                //hacer new CapaDomini.Domini() hace que FrameEditor tenga hmc distinto del de esta funcion
+            } catch (Exception e) {
+                System.out.println("Couldn't do login");
             }
-            
-            
-        });    
-    
+
+        });
+
     }
-    
+
 }
