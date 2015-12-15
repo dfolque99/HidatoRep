04e01888ccc248ca83dbc47c92b1b6ef127b184a
keur <keur@172.16.0.95>
2015-12-15 18:07:34 +0100

    Javadoc

--- Hidato/src/CapaDomini/Tauler/SolverControllerStop.java
+++ Hidato/src/CapaDomini/Tauler/SolverControllerStop.java
@@ -1,32 +1,47 @@
 package CapaDomini.Tauler;
 
 /**
+ * Singleton class providing a value for signaling SolverController class when
+ * the user wants it to stop
  *
  * @author felix.axel.gimeno
  */
 public class SolverControllerStop {
 
+    /**
+     * value stop is false when the SolverController starts, and true if the
+     * user selects "cancel" when solving a hidato
+     * <p>
+     * volatile means threads access the same value
+     */
     private volatile static boolean stop = false;
 
-    //final static SolverControllerStop ref = new SolverControllerStop();
-
+    /**
+     * true if user cancelled solving, false otherwise
+     *
+     * @return value of stop
+     */
     public static boolean isStopped() {
         return SolverControllerStop.stop;
     }
-    public static void allow(){
+
+    /**
+     * make stop value false
+     */
+    public static void allow() {
         SolverControllerStop.stop = false;
     }
 
+    /**
+     * make stop value true
+     */
     public static void stop() {
         SolverControllerStop.stop = true;
     }
 
+    /**
+     * AS this class is a singleton class, it cannot be instantiated
+     */
     private SolverControllerStop() {
     }
-
-    /*
-    public SolverControllerStop getInstance() {
-        return ref;
-    }
-    */
 }
