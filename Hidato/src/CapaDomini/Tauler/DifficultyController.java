e8b3c9e8761353d60121e5ec405f50b81a025fc8
keur <keur@172.16.0.95>
2015-12-15 19:09:12 +0100

    Javadoc

--- Hidato/src/CapaDomini/Tauler/DifficultyController.java
+++ Hidato/src/CapaDomini/Tauler/DifficultyController.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  * To calculate the difficulty of a hidato board
